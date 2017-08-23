3f9d4fa4b246b0ce4c60fede36d6ac677f1902b8
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-08-24 07:56:02 +0800

    update preprocessing

--- Code/preprocessing_twoconvlayers/preprocessing_twolayers.py
+++ Code/preprocessing_twoconvlayers/preprocessing_twolayers.py
@@ -19,14 +19,27 @@ def extract_num_file_in_commit_code(commits, index_commits):
     return ids, fs
 
 
-def commit_code_less_two_files(path_file):
-    # count the number of files in commit code
+def get_commit_commitindex(path_file):
     commits = list(open(path_file, "r").readlines())
     i_commits = commits_index(path_file)
+    return commits, i_commits
+
+
+def commit_code_less_two_files(path_file):
+    # count the number of files in commit code
+    commits, i_commits = get_commit_commitindex(path_file)
     print "Total commits: %i" % (len(i_commits))
     ids, fs = extract_num_file_in_commit_code(commits=commits, index_commits=i_commits)
     greater = [f for f in fs if f > 1]
-    print "Total commits which have two files in commit code: %i" % (len(greater))
+    one = [f for f in fs if f == 1]
+    zero = [f for f in fs if f == 0]
+    print "Total commits which have more than two files in commit code: %i" % (len(greater))
+    print "Total commits which have one file in commit code: %i" % (len(one))
+    print "Total commits which have zero file in commit code: %i" % (len(zero))
+    # for i, j in zip(ids, fs):
+    #     if j == 0:
+    #         print i, j
+    # exit()
     return ids, fs
 
 
@@ -36,10 +49,22 @@ def commit_one_file_commit_code(ids, fs, make_file):
         [file_.write(str(c) + '\n') for c in ids]
     return ids
 
+
+################################################################
+def extract_info_update(path_file, ids):
+    # extract information of commit from in ids (ids: commit id)
+    commits, i_commits = get_commit_commitindex(path_file)
+    # print len(commits), len(i_commits), len(ids)
+
+
+
 ################################################################
 path_file = "../raw_data/eq100_line_aug1.out"
 ids, fs = commit_code_less_two_files(path_file=path_file)
 make_file = "./eq100_line_aug1.out.id_onefile"
-commit_one_file_commit_code(ids, fs, make_file)
+ids = commit_one_file_commit_code(ids, fs, make_file)
+extract_info_update(path_file=path_file, ids=ids)
+
+
 
 
